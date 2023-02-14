c5d0954e4674c7bebf79684ad10b5de20eaa2af2
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-02-13 19:34:14 -0500

    Add files via upload

---
+++ homework 4 question 1.c
@@ -0,0 +1,60 @@
+input('LCM or GCF')
+if('LCM'):
+        x = input('integer 1')
+        y = input('integer 2')
+        
+#calculate least common multiple
+
+while(x):
+    x%2
+count=count+1
+if(y):
+    y=y/2
+print('LCM is', input)
+
+#user input is acceptable 
+   
+if():
+   print('confirm')
+   
+#user input is unacceptable
+
+   while():
+       print('unacceptable')
+   input('a different integer')
+
+while():
+    if('GCF'):
+        input('integer 1')
+    input('integer 2')
+break
+
+    #calculate greatest common factor
+    
+while(x):
+    x%2
+
+if(y):
+    y*2
+    count = count + 1
+    print('GCF is')
+    break
+    
+#user input is acceptable
+
+if():
+    print('confirm')
+    
+#user input is unacceptable
+
+while():
+   print('unacceptable')
+   input('a different integer')
+
+while():
+    print('what do you want to do?')
+input('stop or dont stop')
+break
+
+while(input = stop):
+    EOP
---
+++ homework 4 question 2.c
@@ -0,0 +1,21 @@
+characters = input('200 characters')
+character = input('character')
+count = count + 1
+
+#scan for number of occurrences of specialized character
+
+if(input):
+    count(character)
+
+#characters do not exist in passage
+
+if('character' != characters):
+    print('invalid-enter another character')
+    backup = input('backup character')
+    input(backup)
+       
+if(characters < 200):
+    print('invalid number of characters')
+    passage2 = input('another passage')
+    input(passage2)
+   
